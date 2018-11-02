616da85497320642a42ec8f3468b51efafe5b819
yangchenwangchaonan <946754060@qq.com>
2018-11-02 15:57:56 +0800

    sd

---
+++ Handler1.ashx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1
+{
+    /// <summary>
+    /// Handler1 的摘要说明
+    /// </summary>
+    public class Handler1 : IHttpHandler
+    {
+
+public void ProcessRequest(HttpContext context)
+{
+    if (context.Request.Files.Count > 0)
+    {
+        context.Request.Files[0].SaveAs("d:\\1.wav");
+    }
+}
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
